582186630263f7663deb8082b2c76e4731229c5b
kwangminini <rhkdals7362@gmail.com>
2020-08-19 02:05:03 +0900

    create Out of Setting

--- src/main/java/com/example/springbootgettingstarted/SampleListener.java
+++ src/main/java/com/example/springbootgettingstarted/SampleListener.java
@@ -1,5 +1,7 @@
 package com.example.springbootgettingstarted;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.context.event.ApplicationStartedEvent;
@@ -8,9 +10,16 @@ import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
 @Component
-public class SampleListener implements ApplicationRunner { //application이 실행되고 난 뒤 
+public class SampleListener implements ApplicationRunner { //application이 실행되고 난 뒤
+
+    @Autowired
+    KeesunProperties keesunProperties;
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        System.out.println("args:::"+args);
+        System.out.println("==============");
+        System.out.println(keesunProperties.getName());
+        System.out.println(keesunProperties.getAge());
+        System.out.println("==============");
     }
 }
--- src/test/java/com/example/springbootgettingstarted/SpringBootGettingStartedApplicationTests.java
+++ src/test/java/com/example/springbootgettingstarted/SpringBootGettingStartedApplicationTests.java
@@ -1,13 +1,24 @@
 package com.example.springbootgettingstarted;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
+import org.springframework.test.context.junit4.SpringRunner;
 
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class SpringBootGettingStartedApplicationTests {
 
+	@Autowired
+	Environment environment;
 	@Test
 	void contextLoads() {
+		assertThat(environment.getProperty("keesun.name")).isEqualTo("whiteship");
 	}
 
 }
