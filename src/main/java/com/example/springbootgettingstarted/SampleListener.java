67337c030f4f24f0be3bae0d9189fe6fb30ecb1a
kwangminini <rhkdals7362@gmail.com>
2020-08-18 23:13:45 +0900

    create ApplicationRnunner

---
+++ src/main/java/com/example/springbootgettingstarted/SampleListener.java
@@ -0,0 +1,16 @@
+package com.example.springbootgettingstarted;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.boot.context.event.ApplicationStartingEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SampleListener implements ApplicationRunner { //application이 실행되고 난 뒤 
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.println("args:::"+args);
+    }
+}
